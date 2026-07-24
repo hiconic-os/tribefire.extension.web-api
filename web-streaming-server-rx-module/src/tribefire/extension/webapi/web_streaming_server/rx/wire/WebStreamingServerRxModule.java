8e0bda114a85e462ef21f96f2465b2e481e77eec
tryptamic <dirk.scheffler@gmail.com>
2026-07-24 23:00:45 +0200

    cycle fix

--- web-streaming-server-rx-module/src/tribefire/extension/webapi/web_streaming_server/rx/wire/WebStreamingServerRxModule.java
+++
@@ -1,8 +0,0 @@
-package tribefire.extension.webapi.web_streaming_server.rx.wire;
-
-import hiconic.rx.module.api.wire.RxModule;
-import tribefire.extension.webapi.web_streaming_server.rx.wire.space.WebStreamingServerRxModuleSpace;
-
-public enum WebStreamingServerRxModule implements RxModule<WebStreamingServerRxModuleSpace> {
-	INSTANCE
-}
